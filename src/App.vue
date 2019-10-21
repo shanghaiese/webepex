daf26b2e261fef6cae911a63ce7b4dbc449463bd
squainaly <>
2019-10-21 10:45:12 +0800

    删除了app居中和文字颜色样式

--- src/App.vue
+++ src/App.vue
@@ -16,11 +16,5 @@ export default {
 	font-family: "Avenir", Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-}
-.el-icon-loading,
-.el-loading-text {
-	color: white!important;
 }
 </style>
