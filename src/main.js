4e7d13b54f2d77106d8d0819dc25a63c4c7f5291
squainaly <>
2019-10-25 18:44:15 +0800

    修改iconstyle名字

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ import ElementUI from "element-ui"; // 导入element-ui组件
 import "./theme/element/theme/index.css"; // 导入element-ui主题样式
 import "@/assets/css/normalize.css"; // 导入reset.css重置样式库
 import "@/assets/css/common.css"; // 导入工具类样式
-import "@/assets/css/squain_icon_style.css"; // 导入icon图标样式
+import "@/assets/css/squainicon.css"; // 导入icon图标样式
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
 
