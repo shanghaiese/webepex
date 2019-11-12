a309d83f2b40daa6c8a94ed9dfd2c289b6d9325c
squainaly <>
2019-11-12 15:20:12 +0800

    EPEX98,99,100

--- src/api/base.js
+++ src/api/base.js
@@ -15,6 +15,7 @@ switch(env){
     var url="https://api.e-pex.com"
  }
 }*/
+// var url="http://192.168.18.6:8080";
 var url="https://uat-api.e-pex.com";
 // var url="https://api.e-pex.com";
 export default ({
